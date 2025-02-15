0a25743064f5c26267af4e70fea931cf3a09ab9d
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-02-16 00:49:13 +0530

    MainHome.jsx correct import detaile

--- client/src/components/MainHome.jsx
+++ client/src/components/MainHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../style/MainHome.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import profile from '../assets/home_/profile_i.png';
 
 const MainHome = () => {
