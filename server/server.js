5cd5f8c74de8d0954a7ab736230471ecd59fb48c
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-02-24 02:34:11 +0530

    Modified server.js for socket initialization and route inclusion changes, enhancing real-time messaging functionality -increase limit of 5mb which helps prevent large data payloads from overwhelming the server.(for audio sharing in chat)

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const authRoutes = require('./routes/authRoutes');
 const teamRoutes = require('./routes/teamRoutes');
 const documentRoutes = require('./routes/documentRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const socket = require('./config/socket');
+const http = require("http");
 
 
 dotenv.config(); // Load environment variables
@@ -13,13 +16,15 @@ dotenv.config(); // Load environment variables
 const app = express();
 const port = 4400;
 
+const server = http.createServer(app);
 
 console.log('MongoDB URI:', process.env.URI);
 console.log('CLOUDINARY_API_KEY :', process.env.CLOUDINARY_API_KEY);
 // Middleware
 app.use(cors());
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: "5mb" })); 
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
+
 
 // MongoDB Atlas connection
 const mongoURI = process.env.URI; 
@@ -38,7 +43,11 @@ app.use('/auth', authRoutes);
 app.use('/team', teamRoutes);
 app.use('/file', documentRoutes);
 app.use('/task', taskRoutes);
+app.use('/chat', chatRoutes);
 
-app.listen(port, () => {
+socket.init(server);
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
